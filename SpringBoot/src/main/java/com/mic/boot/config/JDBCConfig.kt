d4405001477205afc1a2fbb53e6e08da4a5ffe34
lpjhblpj@163.com <lpjhblpj@163.com>
2021-10-14 20:24:03 +0800

    yml

--- SpringBoot/src/main/java/com/mic/boot/config/JDBCConfig.kt
+++ SpringBoot/src/main/java/com/mic/boot/config/JDBCConfig.kt
@@ -2,12 +2,16 @@ package com.mic.boot.config
 
 import com.alibaba.druid.pool.DruidDataSource
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.PropertySource
 import javax.sql.DataSource
 
 @Configuration
-@PropertySource("classpath:jdbc.properties")
+//@PropertySource("classpath:jdbc.properties")
+//@EnableConfigurationProperties(JdbcProperties::class)
 open class JDBCConfig{
 
     @Value("${"jdbc.url"}")
@@ -19,12 +23,28 @@ open class JDBCConfig{
     @Value("${"jdbc.password"}")
     var password:String=""
 
-    fun dataSource():DataSource{
-        val ds :DruidDataSource = DruidDataSource()
-        ds.driverClassName=driver
-        ds.url=url
-        ds.username=username
-        ds.password=password
-        return ds
+//    fun dataSource():DataSource{
+//        val ds :DruidDataSource = DruidDataSource()
+//        ds.driverClassName=driver
+//        ds.url=url
+//        ds.username=username
+//        ds.password=password
+//        return ds
+//    }
+
+//    @Bean
+//    open fun dataSource(jdbcProperties: JdbcProperties):DataSource{
+//        val ds :DruidDataSource = DruidDataSource()
+//        ds.driverClassName=jdbcProperties.driverClassName
+//        ds.url=jdbcProperties.url
+//        ds.username=jdbcProperties.username
+//        ds.password=jdbcProperties.password
+//        return ds
+//    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "jdbc")
+    open fun dataSource1():DataSource{
+        return DruidDataSource()
     }
 }
--- SpringBoot/src/main/java/com/mic/boot/controller/BootController.kt
+++ SpringBoot/src/main/java/com/mic/boot/controller/BootController.kt
@@ -1,9 +1,11 @@
 package com.mic.boot.controller
 
 import com.mic.boot.config.JDBCConfig
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import javax.sql.DataSource
 
 @RestController
 open class BootController {
@@ -12,6 +14,15 @@ open class BootController {
          val dbConfig = JDBCConfig()
     }
 
+    @Autowired
+    var dataSourceApp:DataSource?=null
+
+    @Value("\${itcast.url}")
+    private val itcastUrl: String? = null
+
+    @Value("\${itheima.url}")
+    private val itheimaUrl: String? = null
+
     @GetMapping("/getname")
     open fun getName():String{
         return "hello ,Spring Boot"
@@ -19,7 +30,25 @@ open class BootController {
 
     @GetMapping("/dbconfig")
     fun dbConfig():String{
-        return  dbConfig.dataSource().toString()
+//        return  dbConfig.dataSource().toString()
+        return "datasource"
     }
+
+    @GetMapping("/dbconfigapp")
+    fun dbConfigApp():String{
+        return  dataSourceApp.toString()
+    }
+
+    @GetMapping("/dbconfig1")
+    fun dbConfig1():String{
+        return  dbConfig.dataSource1().toString()
+    }
+
+    @GetMapping("/ymlconfig")
+    fun ymlConfig(){
+        println(itcastUrl)
+        println(itheimaUrl)
+    }
+
 
 }

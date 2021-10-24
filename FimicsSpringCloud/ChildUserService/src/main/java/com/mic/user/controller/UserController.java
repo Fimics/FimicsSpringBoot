c2ef824e52423a6590d4aafad09ca40e703858dd
lpjhblpj@163.com <lpjhblpj@163.com>
2021-10-25 01:29:46 +0800

    springcloud bus 及时从git 获取yml配置文件

--- FimicsSpringCloud/ChildUserService/src/main/java/com/mic/user/controller/UserController.java
+++ FimicsSpringCloud/ChildUserService/src/main/java/com/mic/user/controller/UserController.java
@@ -3,6 +3,8 @@ package com.mic.user.controller;
 import com.mic.user.pojo.User;
 import com.mic.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,10 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@RefreshScope //刷新配置  需要用postman 请求一下 127.0.0.1:12000/actuator/bus-refresh
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Value("${test.name}")
+    private String name;
 
     @GetMapping("/{id}")
     public User queryById(@PathVariable long id){
@@ -24,6 +30,7 @@ public class UserController {
 //        } catch (InterruptedException e) {
 //            e.printStackTrace();
 //        }
+        System.out.println("配置文件中的test.name为：" + name);
         return userService.queryById(id);
     }
 }

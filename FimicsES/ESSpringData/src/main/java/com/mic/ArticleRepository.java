554e524c1a328b2f583e665a8849128e0c7059e7
li <lpjhblpj@163.com>
2021-11-16 00:30:08 +0800

    elasticsearch

---
+++ FimicsES/ESSpringData/src/main/java/com/mic/ArticleRepository.java
@@ -0,0 +1,12 @@
+package com.mic;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import java.util.List;
+
+public interface ArticleRepository extends ElasticsearchRepository<Article, Long> {
+    List<Article> findByTitle(String title);
+    List<Article> findByTitleOrContent(String title, String content);
+    List<Article> findByTitleOrContent(String title, String content, Pageable pageable);
+}
